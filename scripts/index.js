5d1a84da8187eaca35e8afd06cdb31dda53451ad
bobr-ik <aavramenko804@gmail.com>
2025-03-14 14:00:24 +0300

    beta version

--- scripts/index.js
+++ scripts/index.js
@@ -29,11 +29,27 @@ showSlide(currentSlide);
 
 
 function openPopup() {
-    const popup = document.querySelector('.popup');
-    popup.style.display = 'flex';
-  }
+  const popup = document.querySelector('.popup');
+  popup.style.display = 'flex';
+}
   
-  function closePopup() {
-    const popup = document.querySelector('.popup');
-    popup.style.display = 'none';
-  }
+function closePopup() {
+  const popup = document.querySelector('.popup');
+  popup.style.display = 'none';
+}
+
+function telegram() {
+    window.open('https://t.me/Vlasova_Boriskovskaya');
+}
+
+function whatsapp() {
+    window.open('https://wa.me/79055377637');
+}
+
+function phone() {
+    window.open('tel:+79055377637');
+}
+
+function navbar_link(page) {
+    window.location.href = page;
+}
